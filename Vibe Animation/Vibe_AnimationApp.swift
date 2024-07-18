64a1225dc550015b55f8eb87bdff298b894a32ee
Hayden Pennington <haydenpennington@icloud.com>
2024-07-17 22:50:54 -0500

    Initial commit

---
+++ Vibe Animation/Vibe_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vibe_AnimationApp.swift
+//  Vibe Animation
+//
+//  Created by Hayden Pennington on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Vibe_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
