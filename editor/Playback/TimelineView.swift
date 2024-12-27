d701e9b9a11fa902a9d7766635c3e1a9bfd35e96
Hayden Pennington <haydenpennington@icloud.com>
2024-12-27 17:54:33 -0600

    Add ruler

--- editor/Playback/TimelineView.swift
+++ editor/Playback/TimelineView.swift
@@ -14,10 +14,48 @@ struct TimelineContainer: View {
         ScrollView {
             HStack {
                 TimelineHierarchy(ids: ["Shape X", "Shape Y", "Shape Z"], isExpanded: $isExpanded)
+                    .padding(.top, 36)
                 
                 Spacer()
                 
                 Timeline {
+                    HStack {
+                        Text("0.0")
+                        Spacer()
+                        Text("0.2")
+                        Spacer()
+                        Text("0.4")
+                        Spacer()
+                        Text("0.6")
+                        Spacer()
+                        Text("0.8")
+                        Spacer()
+                        Text("1.0")
+                        Spacer()
+                        Text("1.2")
+                        Spacer()
+                        Text("1.4")
+                        Spacer()
+                        Text("1.6")
+                        Spacer()
+                        Text("1.8")
+                        Spacer()
+                        Text("2.0")
+                        Spacer()
+                        Text("2.2")
+                        Spacer()
+                        Text("2.4")
+                        Spacer()
+                        Text("2.6")
+                        Spacer()
+                        Text("2.8")
+                        Spacer()
+                        Text("3.0")
+                    }
+                    .frame(maxWidth: .infinity)
+                    
+                    TimelineRuler()
+                    
                     ForEach(["Shape X", "Shape Y", "Shape Z"], id: \.hashValue) { id in
                         TimelineBarBackground()
                             .frame(height: 42)
@@ -27,7 +65,6 @@ struct TimelineContainer: View {
                                 .frame(height: 32)
                         }
                     }
-                    
                 }
             }
         }
@@ -42,8 +79,9 @@ struct Timeline: View {
     }
     
     var body: some View {
-        VStack {
+        VStack(alignment: .center) {
             AnyView(rows())
+                .padding(.horizontal)
             Spacer(minLength: .zero)
         }
         .padding()
